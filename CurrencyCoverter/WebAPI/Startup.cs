5b718e8ab713e147455650d57ec9be4ca9479f4e
Maciej <maciejbie00@gmail.com>
2022-07-02 23:49:51 +0200

    cors fix

--- CurrencyCoverter/WebAPI/Startup.cs
+++ CurrencyCoverter/WebAPI/Startup.cs
@@ -34,6 +34,8 @@ namespace WebAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors();
+
             services.AddHttpClient();
             services.AddScoped<IRateRepository, RateRepository>();
             services.AddScoped<IRateService, RateService>();
@@ -52,6 +54,11 @@ namespace WebAPI
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors(builder => builder
+    .AllowAnyOrigin()
+    .AllowAnyMethod()
+    .AllowAnyHeader());
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
